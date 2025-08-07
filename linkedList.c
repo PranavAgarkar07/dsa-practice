f69eaf2fcf473265e07cc324384811ee7c85d95d
PranavAgarkar07 <pranavagarkar8@gmail.com>
2025-08-08 00:50:44 +0530

    updated

--- linkedList.c
+++ linkedList.c
@@ -6,10 +6,31 @@ struct node
 	struct node* next;
 };
 
+void insert(int item){
+
+	struct node *n{i}=(struct node*)malloc(sizeof(struct node));
+	i++;
+}
+
 void main()
 {
-	struct node *n=(struct node*)malloc(sizeof(struct node));
-	n->data=1;
-	n->next=NULL;
-	printf("%d\n", n->data);
+	struct node *n1=(struct node*)malloc(sizeof(struct node));
+	struct node *n2=(struct node*)malloc(sizeof(struct node));
+	n1->data=1;
+	n1->next=n2;
+	n2->data=2;
+	n2->next=NULL;
+	printf("%d\n", n1->data);
+
+	int ch,item,i;
+
+	while(1){
+		printf("Enter your choice: ")
+		scanf("%d",&ch);
+
+		switch(ch){
+			case 1:
+				break;
+		}
+	}
 }
